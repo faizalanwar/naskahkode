dd4f195826f0fe79375135f1e411639802bc3a97
faizalanwar <fzlanwr@gmail.com>
2022-10-06 19:19:04 +0700

    init

---
+++ data/authors/faizalanwar.md
@@ -0,0 +1,12 @@
+---
+name: Faizal Anwar
+avatar: /static/images/faizalanwar.png
+occupation: Full Stack Developer
+company: naskahkode
+email: fzlanwr@gmail.com
+twitter: https://twitter.com/faizalanwar
+linkedin: https://www.linkedin.com/in/faizalanwar/
+github: https://github.com/faizalanwar
+---
+
+Data Science Engineering student, passionate about science, technologies in general, web development, music production, electricity and electronics, electronic equipment and computer maintenance, internet of things, arts and sports.
---
+++ pages/index.js
@@ -0,0 +1,114 @@
+import Link from '@/components/Link'
+import { PageSEO } from '@/components/SEO'
+import Tag from '@/components/Tag'
+import siteMetadata from '@/data/siteMetadata'
+import { getAllFilesFrontMatter } from '@/lib/mdx'
+import formatDate from '@/lib/utils/formatDate'
+import Image from 'next/image'
+
+import NewsletterForm from '@/components/NewsletterForm'
+
+const MAX_DISPLAY = 5
+
+export async function getStaticProps() {
+  const posts = await getAllFilesFrontMatter('blog')
+
+  return { props: { posts } }
+}
+
+export default function Home({ posts }) {
+  return (
+    <>
+      <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Latest
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p>
+        </div>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          {!posts.length && 'No posts found.'}
+          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+            const { slug, date, title, summary, tags, image } = frontMatter
+            return (
+              <li key={slug} className="py-12">
+                <article>
+                  <div className="flex flex-col items-stretch space-y-2 xl:grid xl:grid-cols-3 xl:items-baseline xl:gap-4 xl:space-y-0">
+                    <div className="w-full space-y-5 self-center">
+                      <Link href={`/blog/${slug}`}>
+                        <Image
+                          className="w-full object-cover hover:brightness-105"
+                          alt={slug}
+                          src={`/static/${image}`}
+                          layout="responsive"
+                          width={800}
+                          height={450}
+                        />
+                      </Link>
+                    </div>
+                    <div className="space-y-5 xl:col-span-2">
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-gray-900 dark:text-gray-100"
+                            >
+                              {title}
+                            </Link>
+                          </h2>
+                          <div className="flex flex-wrap">
+                            {tags.map((tag) => (
+                              <Tag key={tag} text={tag} />
+                            ))}
+                          </div>
+                          <dl>
+                            <dt className="sr-only">Published on</dt>
+                            <dd className="text-xs font-medium leading-6 text-gray-500 dark:text-gray-400">
+                              <time dateTime={date}>{formatDate(date)}</time>
+                            </dd>
+                          </dl>
+                        </div>
+                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                          {summary}
+                        </div>
+                      </div>
+                      <div className="text-base font-medium leading-6">
+                        <Link
+                          href={`/blog/${slug}`}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read "${title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      {posts.length > MAX_DISPLAY && (
+        <div className="flex justify-end text-base font-medium leading-6">
+          <Link
+            href="/blog"
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label="all posts"
+          >
+            All Posts &rarr;
+          </Link>
+        </div>
+      )}
+      {siteMetadata.newsletter.provider !== '' && (
+        <div className="flex items-center justify-center pt-4">
+          <NewsletterForm />
+        </div>
+      )}
+    </>
+  )
+}
