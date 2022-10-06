2447db8a5f8950d57568dd08357d37a1f4a12e68
faizalanwar <fzlanwr@gmail.com>
2022-10-06 19:35:25 +0700

    remove yo

--- components/Footer.js
+++ components/Footer.js
@@ -21,11 +21,7 @@ export default function Footer() {
           <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
-        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
-          <Link href="https://github.com/faizalanwar/naskahkode">
-          yo
-          </Link>
-        </div>
+         
       </div>
     </footer>
   )
