bbee9ab8a8d02f612861807cda345f342cb13bed
faizalanwar <fzlanwr@gmail.com>
2022-10-07 10:12:00 +0700

    add typewriter-effect and react-rough-notation

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -9,8 +9,11 @@ import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 
+import Typewriter from 'typewriter-effect'
+import { useRouter } from 'next/router'
 const LayoutWrapper = ({ children }) => {
   const { theme } = useTheme()
+  const router = useRouter()
 
   return (
     <SectionContainer>
@@ -18,7 +21,7 @@ const LayoutWrapper = ({ children }) => {
         <header className="flex items-center justify-between py-10">
           <div>
             <Link href="/" aria-label={siteMetadata.headerTitle}>
-              <div className="flex items-center justify-between">
+              {/* <div className="flex items-center justify-between">
                 <div className="mr-3">{theme === 'dark' ? <LightLogo /> : <DarkLogo />}</div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
                   <div className="hidden h-6 text-2xl font-semibold sm:block">
@@ -27,6 +30,16 @@ const LayoutWrapper = ({ children }) => {
                 ) : (
                   siteMetadata.headerTitle
                 )}
+              </div> */}
+              <div className="text-primary-color dark:text-primary-color-dark flex items-center justify-between text-xl font-semibold">
+                {`~${router.asPath}`}{' '}
+                <Typewriter
+                  options={{
+                    strings: [],
+                    autoStart: true,
+                    loop: true,
+                  }}
+                />
               </div>
             </Link>
           </div>
