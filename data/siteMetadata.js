fb6b408b16777f36d4cc26dccd2c9dd904c0dc57
faizalanwar <fzlanwr@gmail.com>
2022-10-07 15:19:18 +0700

    update sitemetadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -40,10 +40,15 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+    //   repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
+    //   repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
+    //   category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
+    //   categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
+    
+      repo: "faizalanwar/naskahkode",
+      repositoryId: "R_kgDOIJUmpA",
+      category: "comment",
+      categoryId: "DIC_kwDOIJUmpM4CR2Ep",
       mapping: 'pathname', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
