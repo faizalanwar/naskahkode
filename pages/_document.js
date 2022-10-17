2bf403098a291d239c932516b92ce4139a8292ef
faizalanwar <fzlanwr@gmail.com>
2022-10-17 09:52:06 +0700

    add addsense

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,8 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8855531791863813"
+     crossorigin="anonymous"></script>
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
