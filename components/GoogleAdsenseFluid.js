c4d6df6182be55b48053bd18dc0b56414228d73c
faizalanwar <fzlanwr@gmail.com>
2022-10-22 18:09:35 +0700

    final adsense maybe

--- components/GoogleAdsenseFluid.js
+++ components/GoogleAdsenseFluid.js
@@ -5,7 +5,7 @@ export const AdsenseFluid = () => (
   <Adsense
     client="ca-pub-8855531791863813"
     slot="7523580537"
-    style={{ display: 'block', 'text-align': 'center' }}
+    style={{ display: 'block', textAlign: 'center' }}
     layout="in-article"
     format="fluid"
   />
