a25bda4be5f3174c311d8d7d46209551324bdd05
faizalanwar <fzlanwr@gmail.com>
2022-11-03 12:06:01 +0700

    inde

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,31 @@ import { getAllFilesFrontMatter } from '@/lib/mdx'
 import formatDate from '@/lib/utils/formatDate'
 import Image from 'next/image'
 import Model from '@/components/Model'
-
 import NewsletterForm from '@/components/NewsletterForm'
+import Pagination from '@/components/Pagination'
+import { useState } from 'react'
 
 const MAX_DISPLAY = 5
+export const POSTS_PER_PAGE = 5
 
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('blog')
-
-  return { props: { posts } }
+  const initialDisplayPosts = posts.slice(0, POSTS_PER_PAGE)
+  const pagination = {
+    currentPage: 1,
+    totalPages: Math.ceil(posts.length / POSTS_PER_PAGE),
+  }
+  return { props: { initialDisplayPosts, posts, pagination } }
 }
 
-export default function Home({ posts }) {
+export default function Home({ posts, title, initialDisplayPosts = [], pagination }) {
+  const displayPosts =
+    initialDisplayPosts.length > 0 && !searchValue ? initialDisplayPosts : filteredBlogPosts
+  const [searchValue, setSearchValue] = useState('')
+  const filteredBlogPosts = posts.filter((frontMatter) => {
+    const searchContent = frontMatter.title + frontMatter.summary + frontMatter.tags.join(' ')
+    return searchContent.toLowerCase().includes(searchValue.toLowerCase())
+  })
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
@@ -35,10 +48,37 @@ export default function Home({ posts }) {
             {siteMetadata.description}
           </p>
         </div>
-
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            {title}
+          </h1>
+          <div className="relative max-w-lg">
+            <input
+              aria-label="Search articles"
+              type="text"
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search articles"
+              className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+            />
+            <svg
+              className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+          </div>
+        </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+          {!filteredBlogPosts.length && 'No posts found.'}
+          {displayPosts.map((frontMatter) => {
             const { slug, date, title, summary, tags, image } = frontMatter
             return (
               <li key={slug} className="py-12">
@@ -115,6 +155,9 @@ export default function Home({ posts }) {
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
+      )}
+      {pagination && pagination.totalPages > 1 && !searchValue && (
+        <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
       )}
     </>
   )
